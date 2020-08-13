b0a815d0c09f5a70ebfb02b30cce62fe9e70b119
Yan Savitski <yansavitski@MacBook-Pro-Yan.home>
2020-08-13 18:13:13 +0200

    feat: initial rendering

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ const todos = [
     'Разобраться в замыканиях',
     'Решить задачу на Codewars',
 ]
+const todoListContainer = document.querySelector(".todoList")
+
+todos.forEach(todo => {
+    const templateTodo = `
+        <article class="todoList__item todo">
+            <h5 class="todo__title todo__title_theme_dark">${todo}</h5>
+
+            <div class="todo__controls">
+                <button type="button" class="todo__control control control_edit" />
+                <button type="button" class="todo__control control control_duplicate" />
+                <button type="button" class="todo__control control control_remove" />
+            </div>
+        </article>
+    `
+
+    todoListContainer.insertAdjacentHTML("beforeend", templateTodo)
+})
